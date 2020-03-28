05cc0fdd4f0a92b9f3eca292f498305b0cbe7539
Marcus Wilson <mwilson12388@gmail.com>
2020-03-27 21:35:28 -0400

    New file updates

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ export default {
     },
     cognito: {
       REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_V9DLqcn9FD",
-      APP_CLIENT_ID: "35tik0ffdk4cqg231gp86sbmqe",
-      IDENTITY_POOL_ID: "us-east-1:7e048919-2dbb-4a03-bfdc-430b3851e168 "
+      USER_POOL_ID: "us-east-1_EYUw5LR84",
+      APP_CLIENT_ID: "18hpvjpncupgvupgehdqqo9oj2",
+      IDENTITY_POOL_ID: "us-east-1:8f7b3f11-ad32-49ca-94f2-1a35d88ebfc8"
     }
   };
