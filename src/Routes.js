2b419f06c2286ffebea264c805254f2414a31b75
Marcus Wilson <mwilson12388@gmail.com>
2020-03-27 23:54:29 -0400

    Updated

--- src/Routes.js
+++ src/Routes.js
@@ -3,13 +3,25 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import AppliedRoute from "./components/AppliedRoute";
+import Signup from "./containers/Signup";
+import ResetPassword from "./containers/ResetPassword";
 import NotFound from "./containers/NotFound";
+
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default function Routes({ appProps }) {
   return (
     <Switch>
       <AppliedRoute path="/" exact component={Home} appProps={appProps} />
-      <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <UnauthenticatedRoute path="/login" exact component={Login} appProps={appProps} />
+      <UnauthenticatedRoute path="/signup" exact component={Signup} appProps={appProps} />
+      <AppliedRoute
+        path="/login/reset"
+        exact
+        component={ResetPassword}
+        appProps={appProps}
+      />
       { /* Finally, catch all unmatched routes */ }
       <Route component={NotFound} />
     </Switch>
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,28 +1,31 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Auth } from "aws-amplify";
 import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import LoaderButton from "../components/LoaderButton";
-import 'crypto-js/lib-typedarrays';
-import { Auth } from 'aws-amplify';
+import { useFormFields } from "../libs/hooksLib";
 import "./Login.css";
 
 export default function Login(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: ""
+  });
 
   function validateForm() {
-    return email.length > 0 && password.length > 0;
+    return fields.email.length > 0 && fields.password.length > 0;
   }
 
   async function handleSubmit(event) {
     event.preventDefault();
-  
+
     setIsLoading(true);
-  
+
     try {
-      await Auth.signIn(email, password);
+      await Auth.signIn(fields.email, fields.password);
       props.userHasAuthenticated(true);
-      props.history.push("/");
+
     } catch (e) {
       alert(e.message);
       setIsLoading(false);
@@ -37,25 +40,26 @@ export default function Login(props) {
           <FormControl
             autoFocus
             type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
+            value={fields.email}
+            onChange={handleFieldChange}
           />
         </FormGroup>
         <FormGroup controlId="password" bsSize="large">
           <ControlLabel>Password</ControlLabel>
           <FormControl
-            value={password}
-            onChange={e => setPassword(e.target.value)}
             type="password"
+            value={fields.password}
+            onChange={handleFieldChange}
           />
         </FormGroup>
+        <Link to="/login/reset">Forgot password?</Link>
         <LoaderButton
           block
           type="submit"
           bsSize="large"
           isLoading={isLoading}
           disabled={!validateForm()}
-          >
+        >
           Login
         </LoaderButton>
       </form>
