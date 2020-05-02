bfce7bca91b908a0c3bd5322eb01e2a4200d66e3
Marcus Wilson <mwilson12388@gmail.com>
2020-05-02 15:32:15 -0400

    small edit to reportId variable

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,102 +8,6 @@ export default function Home(props) {
   const [reports, setReports] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  const AWS = require("aws-sdk");
-  let group = [];
-  let groups = [];
-  let groupName =[];
-  
-
-
-  const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({
-
-    region: 'us-east-1',
-    credentials: {
-      accessKeyId: 'AKIAJMUCAALBIX7B2YFQ',
-      secretAccessKey: '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'
-    }
-
-  });
-
-  var params = {
-
-    UserPoolId: 'us-east-1_EYUw5LR84'
-  }
-
- 
-function getGroups() {
-  cognitoidentityserviceprovider.listGroups(params, function(err, data) {
-    var response = JSON.stringify(data);
-    var list = JSON.parse(response);
-    var tmp = null;
-    
-      tmp = list.Groups[2].GroupName;
-     // console.log(tmp);
-      group.push(tmp);
-      console.log(group[0]);
-      groups = group.map((element) => {
-        return element;
-      })
-      //console.log(groups[0]);
-      
-     
-   
-    }
-    
-  )
-  console.log(groups);
-}
-
-
-
-
-/* Auth.currentAuthenticatedUser().then(function(response) {
-      var output = JSON.stringify(response);
-      var dis = JSON.parse(output);
-      var real = (dis.username).toString();
-    
-
-    var params = {
-
-      UserPoolId: 'us-east-1_EYUw5LR84', 
-      Username: real
-    }
-  
-    cognitoidentityserviceprovider.adminListGroupsForUser(params, function(err, data) {
-  
-      var result = JSON.stringify(data);
-      var json = JSON.parse(result);
-  
-      var groupName = (json.Groups[0].GroupName);
-
-      return(
-        <h1>
-          {'Group: ' + groupName }
-        </h1>
-      );
-  
-    })
-  }
-
-) */
-
-  /* var params = {
-
-    UserPoolId: 'us-east-1_EYUw5LR84', 
-    Username: ''
-  }
-
-  cognitoidentityserviceprovider.adminListGroupsForUser(params, function(err, data) {
-
-    var result = JSON.stringify(data);
-    var json = JSON.parse(result);
-
-    var groupName = (json.Groups[0].GroupName);
-
-  }); */
-
-
-
   useEffect(() => {
     async function onLoad() {
       if (!props.isAuthenticated) {
@@ -130,7 +34,7 @@ function getGroups() {
   function renderReportsList(reports) {
     return [{}].concat(reports).map((report, i) =>
       i !== 0 ? (
-        <LinkContainer key={report.reportID} to={`/reports/${report.reportID}`}>
+        <LinkContainer key={report.reportId} to={`/reports/${report.reportId}`}>
           <ListGroupItem header={report.content.trim().split("\n")[0]}>
             {"Created: " + new Date(report.createdAt).toLocaleString()}
           </ListGroupItem>
@@ -174,8 +78,6 @@ function getGroups() {
   return (
     <div className="Home">
       { props.isAuthenticated ? renderReports() : renderLander() }
-      { getGroups() }
-     
     </div>
   );
   
